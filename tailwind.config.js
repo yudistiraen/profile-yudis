d09c98b4a0b2e4592da93208cb9b02d14950fc4c
yudistiraen <yudistiradoremi@gmail.com>
2021-09-20 10:35:53 +0700

    creating tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  purge: [
+    './src/components/**/*.{vue,js}',
+    './src/layouts/**/*.vue',
+    './src/pages/**/*.vue',
+    './src/plugins/**/*.{js,ts}',
+    './nuxt.config.{js,ts}',
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
